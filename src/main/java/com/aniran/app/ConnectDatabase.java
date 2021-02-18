1307d4266434b88a2015374c98eb3c146991925f
Aniran <aniranjose@gmail.com>
2021-02-18 13:35:11 +0100

    - Connection static.

--- src/main/java/com/aniran/app/ConnectDatabase.java
+++ src/main/java/com/aniran/app/ConnectDatabase.java
@@ -6,33 +6,28 @@ import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
 
 public class ConnectDatabase {
-    private static Connection con = null;
+    private static Connection con;
 //    private static Formatter stringFmt = new Formatter(new StringBuilder());
     private static final String insertQry = "INSERT INTO log_event " +
             "(id, timestamp_start, timestamp_finish, duration, host, type) " +
             "values (%s)";
 
-    public static Connection newConnection() {
+    static {
+        try {
+            //Registering the HSQLDB JDBC driver
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+            //Creating the connection with HSQLDB
+            ConnectDatabase.con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost:9001/log_events", "SA", "");
 
-        if (ConnectDatabase.con == null){
-            try {
-                //Registering the HSQLDB JDBC driver
-                Class.forName("org.hsqldb.jdbc.JDBCDriver");
-                //Creating the connection with HSQLDB
-                ConnectDatabase.con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost:9001/log_events", "SA", "");
-
-                if (ConnectDatabase.con != null){
-                    System.out.println("Connection created successfully");
-                }else{
-                    System.out.println("Problem with creating connection");
-                }
-
-            }  catch (Exception e) {
-                e.printStackTrace(System.out);
+            if (ConnectDatabase.con != null){
+                System.out.println("Connection created successfully");
+            }else{
+                System.out.println("Problem with creating connection");
             }
-        }
 
-        return ConnectDatabase.con;
+        }  catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
     }
 
     public static int insertLogEvent(LogEvent logEvent){
